3c00e496910902ce008d4bf1d72c8a887a91213f
11517k <n.kobayashi1215@gmail.com>
2024-05-09 17:32:27 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana, presence: true
+
+  # 全角のバリデーション
+  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
+
+  # カタカナのバリデーション
+  validates :first_name_kana, :last_name_kana,
+            format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
+
+  # パスワードのバリデーション
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
 end
