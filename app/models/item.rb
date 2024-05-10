bb7d45b17d92c732d5c7368eb95cb240cecbf6a7
11517k <n.kobayashi1215@gmail.com>
2024-05-10 16:57:00 +0900

    ActiveStorage、ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_condition, :category, :shipping_fee_payer, :prefecture, :days_to_ship
+
+  validates :image, :item_name, :description presence: true
+  validates :category_id, :item_condition_id, :shipping_fee_payer_id, 
+            :prefecture_id, :days_to_ship_id  numericality: { other_than: 1 } 
+end
+
 end
