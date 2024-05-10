c5e6693a106c60593c45e1594e0e2753357a9437
11517k <n.kobayashi1215@gmail.com>
2024-05-10 17:13:53 +0900

    バリデーションの設定、ログインしていない場合のリダイレクト

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,7 @@ class Item < ApplicationRecord
   belongs_to :item_condition, :category, :shipping_fee_payer, :prefecture, :days_to_ship
 
   validates :image, :item_name, :description presence: true
-  validates :category_id, :item_condition_id, :shipping_fee_payer_id, 
-            :prefecture_id, :days_to_ship_id  numericality: { other_than: 1 } 
+  validates :category_id, :item_condition_id, :shipping_fee_payer_id, :prefecture_id, :days_to_ship_id  numericality: { other_than: 1 } 
 end
 
 end
