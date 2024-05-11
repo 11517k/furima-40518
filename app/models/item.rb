4e7bc663fc7c870a17a60c69f70c7796b97627c1
11517k <n.kobayashi1215@gmail.com>
2024-05-11 19:01:51 +0900

    商品出品ページを表示、ログインしていない場合のリダイレクト

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,12 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :item_condition, :category, :shipping_fee_payer, :prefecture, :days_to_ship
+  belongs_to :item_condition
+  belongs_to :category
+  belongs_to :shipping_fee_payer
+  belongs_to :prefecture
+  belongs_to :days_to_ship
 
-  validates :image, :item_name, :description presence: true
-  validates :category_id, :item_condition_id, :shipping_fee_payer_id, :prefecture_id, :days_to_ship_id  numericality: { other_than: 1 } 
-end
-
+  validates :image, :item_name, :description, presence: true
+  validates :category_id, :item_condition_id, :shipping_fee_payer_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 } 
 end
