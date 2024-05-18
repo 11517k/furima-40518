4b6150c933f8ec931996242832116da9e9bb26e0
11517k <n.kobayashi1215@gmail.com>
2024-05-18 17:29:13 +0900

    テストコードの実装

--- spec/factories/delivery_addresses.rb
+++ spec/factories/delivery_addresses.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :delivery_address do
     postal_code       { '123-4567' }
     prefecture_id     { 2 }  # あなたの都道府県データの中の有効なID
-    city_ward_town    { '東京都渋谷区' }
-    street_address    { '青山1-2-3' }
-    building_name     { '青山ビル101' }
+    city    { '東京都渋谷区' }
+    address    { '青山1-2-3' }
+    building     { '青山ビル101' }
     phone_number      { '09012345678' }
     association       :purchase_record
   end
--- spec/models/delivery_address_spec.rb
+++ spec/models/delivery_address_spec.rb
@@ -5,54 +5,78 @@ RSpec.describe PurchaseDelivery, type: :model do
     @purchase_delivery = FactoryBot.build(:purchase_delivery)
   end
 
-  it 'すべてのフィールドが存在すれば保存できること' do
-    expect(@purchase_delivery).to be_valid
-  end
+  describe '配送先情報の保存' do
+    context '内容に問題がない場合' do
+      it 'すべての項目が正しく入力されていれば保存できる' do
+        expect(@purchase_delivery).to be_valid
+      end
 
-  it '郵便番号が空では保存できないこと' do
-    @purchase_delivery.postal_code = nil
-    @purchase_delivery.valid?
-    expect(@purchase_delivery.errors.full_messages).to include("Postal code can't be blank")
-  end
+      it '建物名がなくても保存できる' do
+        @purchase_delivery.building = ''
+        expect(@purchase_delivery).to be_valid
+      end
+    end
 
-  it '郵便番号は3桁-4桁の形式でないと保存できないこと' do
-    @purchase_delivery.postal_code = '1234567'
-    @purchase_delivery.valid?
-    expect(@purchase_delivery.errors.full_messages).to include('Postal code is invalid')
-  end
+    context '内容に問題がある場合' do
+      it '郵便番号が空では保存できない' do
+        @purchase_delivery.postal_code = ''
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("Postal code can't be blank")
+      end
 
-  it '都道府県が空では保存できないこと' do
-    @purchase_delivery.prefecture_id = nil
-    @purchase_delivery.valid?
-    expect(@purchase_delivery.errors.full_messages).to include("Prefecture can't be blank")
-  end
+      it '郵便番号が「3桁ハイフン4桁」の半角文字列でないと保存できない' do
+        @purchase_delivery.postal_code = '1234567'
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      end
 
-  it '市区町村が空では保存できないこと' do
-    @purchase_delivery.city_ward_town = nil
-    @purchase_delivery.valid?
-    expect(@purchase_delivery.errors.full_messages).to include("City ward town can't be blank")
-  end
+      it '都道府県が1では保存できない' do
+        @purchase_delivery.prefecture_id = 1
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("Prefecture must be other than 1")
+      end
 
-  it '番地が空では保存できないこと' do
-    @purchase_delivery.street_address = nil
-    @purchase_delivery.valid?
-    expect(@purchase_delivery.errors.full_messages).to include("Street address can't be blank")
-  end
+      it '市区町村が空では保存できない' do
+        @purchase_delivery.city = ''
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("City can't be blank")
+      end
 
-  it '建物名は空でも保存できること' do
-    @purchase_delivery.building_name = nil
-    expect(@purchase_delivery).to be_valid
-  end
+      it '番地が空では保存できない' do
+        @purchase_delivery.address = ''
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("Address can't be blank")
+      end
 
-  it '電話番号が空では保存できないこと' do
-    @purchase_delivery.phone_number = nil
-    @purchase_delivery.valid?
-    expect(@purchase_delivery.errors.full_messages).to include("Phone number can't be blank")
-  end
+      it '電話番号が空では保存できない' do
+        @purchase_delivery.phone_number = ''
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("Phone number can't be blank")
+      end
 
-  it '電話番号が10桁以上11桁以内でなければ保存できないこと' do
-    @purchase_delivery.phone_number = '090123456789'
-    @purchase_delivery.valid?
-    expect(@purchase_delivery.errors.full_messages).to include('Phone number is too long (maximum is 11 characters)')
+      it '電話番号が10桁未満では保存できない' do
+        @purchase_delivery.phone_number = '090123456'
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("Phone number is too short")
+      end
+
+      it '電話番号が12桁以上では保存できない' do
+        @purchase_delivery.phone_number = '090123456789'
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("Phone number is invalid. Input only number")
+      end
+
+      it '電話番号がハイフンを含むと保存できない' do
+        @purchase_delivery.phone_number = '090-1234-5678'
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("Phone number is invalid. Input only number")
+      end
+
+      it 'トークンが空では保存できない' do
+        @purchase_delivery.token = ''
+        @purchase_delivery.valid?
+        expect(@purchase_delivery.errors.full_messages).to include("Token can't be blank")
+      end
+    end
   end
 end
